0fbe4ccd0f3d89fafedad0bb3e0707df95442cd6
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-25 22:58:07 -0600

    Adding workouts to seeds

--- seeds/galleryData.js
+++ seeds/galleryData.js
@@ -2,22 +2,22 @@ const { Gallery } = require('../models');
 
 const gallerydata = [
   {
-    name: 'Printemps',
+    name: 'Abs',
     starting_date: 'April 20, 2021 07:00:00',
     ending_date: 'June 21, 2021 17:00:00',
   },
   {
-    name: 'Sommer',
+    name: 'Upper-Body',
     starting_date: 'June 22, 2021 09:00:00',
     ending_date: 'September 22, 2021 22:00:00',
   },
   {
-    name: 'Herfst',
+    name: 'Legs',
     starting_date: 'September 23, 2021 08:30:00',
     ending_date: 'December 21, 2021 20:30:00',
   },
   {
-    name: 'Invierno',
+    name: 'Cardio',
     starting_date: 'December 22, 2020 11:00:00',
     ending_date: 'March 19, 2021 19:00:00',
   },
