d39686430919d7187fdb2b163a4d2fad7723b92c
kirchnerdev22 <trentonlkirchner@gmail.com>
2023-01-26 12:24:13 -0600

    session is now working, it is being documented on the login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,9 +17,21 @@ router.get('/', async (req, res) => {
       gallery.get({ plain: true })
     );
 
-    res.render('homepage', {
-      galleries,
-      loggedIn: req.session.loggedIn,
+    req.session.save(() => {
+      // We set up a session variable to count the number of times we visit the homepage
+      if (req.session.countVisit) {
+        // If the 'countVisit' session variable already exists, increment it by 1
+        req.session.countVisit++;
+      } else {
+        // If the 'countVisit' session variable doesn't exist, set it to 1
+        req.session.countVisit = 1;
+      }
+
+      res.render('homepage', {
+        galleries,
+        // We send over the current 'countVisit' session variable to be rendered
+        countVisit: req.session.countVisit,
+      });
     });
   } catch (err) {
     console.log(err);
