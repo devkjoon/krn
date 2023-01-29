60c6331f3751c471489ef164979dd54ab7af15ec
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-29 12:07:08 -0600

    Working to resolve errors

--- controllers/api/bmi-routes.js
+++ controllers/api/bmi-routes.js
@@ -1,52 +1,45 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const bmi = require('../../models/bmi');
-
-router.use(function(req, res, next) {
-  console.log("Router middleware is being executed");
-  next();
-});
 
 // GET BMI
 router.get(`/bmi/:bmi`, async (req, res) => {
-    // If the user is not logged in, redirect the user to the login page
-    if (!req.session.loggedIn) {
-    res.redirect('/login');
-    } else {
-    // If the user is logged in, allow them to view the Exercises
-    try {
-      const userInput = req.params.bmi.split('-')
-    const bmiurl = `https://fitness-calculator.p.rapidapi.com/bmi?age=${userInput[0]}&weight=${userInput[1]}&height=${userInput[2]}`;
-    const bmioptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
-      'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
-    }
-    };
-    const response = await fetch(bmiurl, bmioptions);
-    let stats = await response.json();
-    stats = stats.data
-    console.log(stats);
-    res.render('bmi', { stats, loggedIn: req.session.loggedIn });
-    } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-    }
-    }
-    });
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    return res.redirect('/login');
+  }
 
-    router.post('/', async (req, res) => {
-        try { 
-          const bmiData = await bmi.create({
-          age: req.body.age,
-          height: req.body.height,
-          weight: req.body.weight,
-        });
-        res.status(200).json(bmiData)
-      } catch (err) {
-        res.status(400).json(err);
+  try {
+    const userInput = req.params.bmi.split('-');
+    const bmiUrl = `https://fitness-calculator.p.rapidapi.com/bmi?age=${userInput[0]}&weight=${userInput[1]}&height=${userInput[2]}`;
+    const bmiOptions = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
+        'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
       }
-      });
-  
-  module.exports = router;
+    };
+    const response = await fetch(bmiUrl, bmiOptions);
+    let stats = await response.json();
+    stats = stats.data;
+    res.render('bmi', { stats, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
 
+router.post('/', async (req, res) => {
+  try { 
+    const bmiData = await bmi.create({
+      age: req.body.age,
+      height: req.body.height,
+      weight: req.body.weight,
+    });
+    res.status(200).json(bmiData);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+module.exports = router;
