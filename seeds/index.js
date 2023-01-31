576269dd13d0a210e958941eb4b44d20fdd02aa3
Liam Allen <liamallen343@gmail.com>
2023-01-30 18:45:43 -0600

    Fixed exercise fetch urls

--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,7 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedWorkout();
+  // await seedWorkout();
 
   // await seedExercises();
 
