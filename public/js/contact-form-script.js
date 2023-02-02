e9e67bf4a1c1af0ebdbb63ac29f33957505d15fc
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-02-01 18:07:45 -0600

    Working on email

--- public/js/contact-form-script.js
+++ public/js/contact-form-script.js
@@ -1,73 +1,73 @@
 //Alexandria turned this off to test if this works for the contact section on the homepage - let me know if it goes to something else
-// $("#contactForm")
-//   .validator()
-//   .on("submit", function (event) {
-//     if (event.isDefaultPrevented()) {
-//       // handle the invalid form...
-//       formError();
-//       submitMSG(false, "Did you fill in the form properly?");
-//     } else {
-//       // everything looks good!
-//       event.preventDefault();
-//       submitForm();
-//     }
-//   });
+$("#contactForm")
+  .validator()
+  .on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+      // handle the invalid form...
+      formError();
+      submitMSG(false, "Did you fill in the form properly?");
+    } else {
+      // everything looks good!
+      event.preventDefault();
+      submitForm();
+    }
+  });
 
-// function submitForm() {
-//   // Initiate Variables With Form ContentF
-//   var name = $("#name").val();
-//   var email = $("#email").val();
-//   var msg_subject = $("#msg_subject").val();
-//   var message = $("#message").val();
+function submitForm() {
+  // Initiate Variables With Form ContentF
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var msg_subject = $("#msg_subject").val();
+  var message = $("#message").val();
 
-//   $.ajax({
-//     type: "POST",
-//     url: "php/form-process.php",
-//     data:
-//       "name=" +
-//       name +
-//       "&email=" +
-//       email +
-//       "&msg_subject=" +
-//       msg_subject +
-//       "&message=" +
-//       message,
-//     success: function (text) {
-//       if (text == "success") {
-//         formSuccess();
-//       } else {
-//         formError();
-//         submitMSG(false, text);
-//       }
-//     },
-//   });
-// }
+  $.ajax({
+    type: "POST",
+    url: "php/form-process.php",
+    data:
+      "name=" +
+      name +
+      "&email=" +
+      email +
+      "&msg_subject=" +
+      msg_subject +
+      "&message=" +
+      message,
+    success: function (text) {
+      if (text == "success") {
+        formSuccess();
+      } else {
+        formError();
+        submitMSG(false, text);
+      }
+    },
+  });
+}
 
-// function formSuccess() {
-//   $("#contactForm")[0].reset();
-//   submitMSG(true, "Message Submitted!");
-// }
+function formSuccess() {
+  $("#contactForm")[0].reset();
+  submitMSG(true, "Message Submitted!");
+}
 
-// function formError() {
-//   $("#contactForm")
-//     .removeClass()
-//     .addClass("shake animated")
-//     .one(
-//       "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
-//       function () {
-//         $(this).removeClass();
-//       }
-//     );
-// }
+function formError() {
+  $("#contactForm")
+    .removeClass()
+    .addClass("shake animated")
+    .one(
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+      function () {
+        $(this).removeClass();
+      }
+    );
+}
 
-// function submitMSG(valid, msg) {
-//   if (valid) {
-//     var msgClasses = "h3 text-center tada animated text-success";
-//   } else {
-//     var msgClasses = "h3 text-center text-danger";
-//   }
-//   $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-// }
+function submitMSG(valid, msg) {
+  if (valid) {
+    var msgClasses = "h3 text-center tada animated text-success";
+  } else {
+    var msgClasses = "h3 text-center text-danger";
+  }
+  $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
 
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
