31b57d04b7061b04604c4dc4db8dd83d14039eeb
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-02-01 18:05:38 -0600

    Working on email

--- public/js/contact-form-script.js
+++ public/js/contact-form-script.js
@@ -1,3 +1,95 @@
+//Alexandria turned this off to test if this works for the contact section on the homepage - let me know if it goes to something else
+// $("#contactForm")
+//   .validator()
+//   .on("submit", function (event) {
+//     if (event.isDefaultPrevented()) {
+//       // handle the invalid form...
+//       formError();
+//       submitMSG(false, "Did you fill in the form properly?");
+//     } else {
+//       // everything looks good!
+//       event.preventDefault();
+//       submitForm();
+//     }
+//   });
+
+// function submitForm() {
+//   // Initiate Variables With Form ContentF
+//   var name = $("#name").val();
+//   var email = $("#email").val();
+//   var msg_subject = $("#msg_subject").val();
+//   var message = $("#message").val();
+
+//   $.ajax({
+//     type: "POST",
+//     url: "php/form-process.php",
+//     data:
+//       "name=" +
+//       name +
+//       "&email=" +
+//       email +
+//       "&msg_subject=" +
+//       msg_subject +
+//       "&message=" +
+//       message,
+//     success: function (text) {
+//       if (text == "success") {
+//         formSuccess();
+//       } else {
+//         formError();
+//         submitMSG(false, text);
+//       }
+//     },
+//   });
+// }
+
+// function formSuccess() {
+//   $("#contactForm")[0].reset();
+//   submitMSG(true, "Message Submitted!");
+// }
+
+// function formError() {
+//   $("#contactForm")
+//     .removeClass()
+//     .addClass("shake animated")
+//     .one(
+//       "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+//       function () {
+//         $(this).removeClass();
+//       }
+//     );
+// }
+
+// function submitMSG(valid, msg) {
+//   if (valid) {
+//     var msgClasses = "h3 text-center tada animated text-success";
+//   } else {
+//     var msgClasses = "h3 text-center text-danger";
+//   }
+//   $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+// }
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+javascript
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'krnhealthwelness.com', // Change to your recipient
+  from: 'krnhealthwelness.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
 $("#contactForm")
   .validator()
   .on("submit", function (event) {
@@ -14,32 +106,24 @@ $("#contactForm")
 
 function submitForm() {
   // Initiate Variables With Form ContentF
-  var name = $("#name").val();
-  var email = $("#email").val();
-  var msg_subject = $("#msg_subject").val();
-  var message = $("#message").val();
+  let name = $("#name").val();
+  let email = $("#email").val();
+  let phone = $("#phone").val();
+  let message = $("#message").val();
+  let krnEmail = {
+    to: `krnhealthwellness@gmail.com`,
+    from: `${email}`,
+    subject: 'Admin Account set up',
+    text:
+      `Hi KRN,
 
-  $.ajax({
-    type: "POST",
-    url: "php/form-process.php",
-    data:
-      "name=" +
-      name +
-      "&email=" +
-      email +
-      "&msg_subject=" +
-      msg_subject +
-      "&message=" +
-      message,
-    success: function (text) {
-      if (text == "success") {
-        formSuccess();
-      } else {
-        formError();
-        submitMSG(false, text);
-      }
-    },
-  });
+${message}
+
+Sincerely,
+${name}
+${phone}`
+  }
+
 }
 
 function formSuccess() {
@@ -59,11 +143,3 @@ function formError() {
     );
 }
 
-function submitMSG(valid, msg) {
-  if (valid) {
-    var msgClasses = "h3 text-center tada animated text-success";
-  } else {
-    var msgClasses = "h3 text-center text-danger";
-  }
-  $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-}
