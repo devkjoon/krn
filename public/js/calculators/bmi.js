84fb729c035d310998304d4c5a1feed6e2b485a0
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-02-01 20:01:03 -0600

    Working on calorie intake

--- public/js/calculators/bmi.js
+++ public/js/calculators/bmi.js
@@ -11,75 +11,66 @@ async function oneRepMax(e) {
   return result
 }
 
-async function basalMetabolicRate(e) {
-  e.preventDefault();
+// async function basalMetabolicRate(e) {
+//   e.preventDefault();
 
-  const height = document.querySelector('#height2').value;
-  const weight = document.querySelector('#weight3').value;
-  const age = document.querySelector('#age2').value;
+//   const height = document.querySelector('#height2').value;
+//   const weight = document.querySelector('#weight3').value;
+//   const age = document.querySelector('#age2').value;
 
-  const bmr = BMR_men(weight, height, age)
-  return bmr
-}
+//   const bmr = BMR_men(weight, height, age)
+//   return bmr
+// }
 
-async function idealBodyWeight(e) {
-  e.preventDefault();
+// async function idealBodyWeight(e) {
+//   e.preventDefault();
 
-  const height = document.querySelector('#height3').value;
-  const age = document.querySelector('#age3').value;
+//   const height = document.querySelector('#height3').value;
+//   const age = document.querySelector('#age3').value;
 
-  const ibw =  IBW_men(height, age);
-  return ibw
-}
+//   const ibw =  IBW_men(height, age);
+//   return ibw
+// }
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
+    'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+  }
+};
+
+const options2 = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
+    'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+  }
+};
+
+const healthDiv = `<div id="healthWrap"></div>`
 
 async function newFormHandler(event) {
   event.preventDefault();
   const age = parseInt(document.querySelector('#age').value);
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
-  // window.location=`/bmi/${age}-${weight}-${height}`;
+  const gender = document.querySelector('#gender').value;
+  const neck = parseInt(document.querySelector('#neck').value);
+  const waist = parseInt(document.querySelector('#waist').value);
+  const hips = parseInt(document.querySelector('#hips').value);
+  const activitylevel = document.querySelector('#activitylevel').value;
   window.location = `/bmi/${age}/${weight}/${height}`;
-  // const bmiURL = `https://fitness-calculator.p.rapidapi.com/bmi?age=${age}&weight=${weight}&height=${height}`;
-  // const bmi = {
-  //   method: 'GET',
-  //   headers: {
-  //     'X-RapidAPI-Key': '06ed075c2bmsh028e8aca739c630p1050c5jsn0053a426db18',
-  //     'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
-    // }
-  // };
 
-  // fetch(bmiURL, bmi)
-  //   .then(res => res.json())
-  //   .then(async json => {
-  //     console.log(json);
-  //    // Send fetch request 
-  //     try{
-  //     const response = await fetch(`/bmi`, {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         age,
-  //         height,
-  //         weight,
-  //       }),
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
-  //     if (response.ok) {
-  //       document.location.replace('/');
-  //     } else {
-  //       alert('Issue loading results');
-  //     }
-  //   } catch(err) {
-  //     console.log(err);
-  //   }
-  //   })
-  //   .catch(err => {
-  //     console.error('error:' + err)
-  //   });
 }
 
+
+
+
+
+
 document.querySelector(".bmiInput").addEventListener("submit", newFormHandler);
-document.querySelector(".ormInput").addEventListener("submit", newFormHandler);
-document.querySelector(".bmrInput").addEventListener("submit", newFormHandler);
-document.querySelector(".ibwInput").addEventListener("submit", newFormHandler);
+// document.querySelector(".ormInput").addEventListener("submit", newFormHandler);
+// document.querySelector(".bmrInput").addEventListener("submit", newFormHandler);
+// document.querySelector(".ibwInput").addEventListener("submit", newFormHandler);
