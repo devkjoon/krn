c08745c5cb693ecb9db7c1c5aaaa4b2ddf92034d
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-02-01 21:39:18 -0600

    Calorie intake is working. Need to fix the div so it is not wiping out bmi

--- public/js/calculators/bmi.js
+++ public/js/calculators/bmi.js
@@ -6,6 +6,8 @@ const options = {
   }
 };
 
+
+
 // const weight = (req.params.weight / 2.205)
 // const height = (req.params.height * 2.54)
 
@@ -28,14 +30,13 @@ function bmiInput(event) {
   const height = parseInt(document.querySelector('#height').value) * 2.54;
   const weight = parseInt(document.querySelector('#weight').value) / 2.205;
   let url = `https://fitness-calculator.p.rapidapi.com/bmi?age=${age}&weight=${weight}&height=${height}`;
-
   fetch(url, options)
   .then(res => res.json())
   .then(json => {
     
     console.log(json)
 
-    $("#bmiCont").empty()
+    // $("#bmiCont").empty()
     $("#bmiCont").append(bmiDiv)
     
     
@@ -51,6 +52,33 @@ function bmiInput(event) {
     $("#bmiWrap").append(bmiContent)
 })}
 
+function calIntakeInput(event) {
+  event.preventDefault();
+  const age = parseInt(document.querySelector('#age').value);
+  const gender = document.querySelector('#gender').value;
+  const height = parseInt(document.querySelector('#height').value) * 2.54;
+  const weight = parseInt(document.querySelector('#weight').value) / 2.205;
+  const activitylevel = document.querySelector('#activitylevel').value;
+  let calorieintakeurl = `https://fitness-calculator.p.rapidapi.com/dailycalorie?age=${age}&gender=${gender}&height=${height}&weight=${weight}&activitylevel=${activitylevel}`
+
+  fetch(calorieintakeurl, options)
+  .then(res => res.json())
+  .then(json => {
+    
+    console.log(json)
+    $("#bmiCont").append(bmiDiv)
+    
+    //bmi content is getting wiped out. Need to change the div. Project for Thursday :) 
+    const bmiContent = 
+    `<div class="bmiCard">
+    <p class="bmrValue">${json.data.BMR}</p>
+    <p class="goalsValue">${json.data.goals[0]}</p>
+    </div>`
+
+    console.log(bmiContent)
+
+    $("#bmiWrap").append(bmiContent)
+})}
 // async function basalMetabolicRate(e) {
 //   e.preventDefault();
 
@@ -96,7 +124,7 @@ function bmiInput(event) {
 
 
 
-$(".bmiInputs").on("submit", bmiInput);
+$(".bmiInputs").on("submit", bmiInput, calIntakeInput);
 // document.querySelector(".ormInput").addEventListener("submit", newFormHandler);
 // document.querySelector(".bmrInput").addEventListener("submit", newFormHandler);
 // document.querySelector(".ibwInput").addEventListener("submit", newFormHandler);
