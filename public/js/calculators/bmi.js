6a0811b2df1e17366ca582e668bbe99adfa24a1f
kirchnerdev22 <trentonlkirchner@gmail.com>
2023-02-02 11:46:56 -0600

    bmi is now functioning, just need to have it show the last two inputs on the page

--- public/js/calculators/bmi.js
+++ public/js/calculators/bmi.js
@@ -1,3 +1,5 @@
+// const { STRING } = require("sequelize");
+
 const options = {
   method: 'GET',
   headers: {
@@ -5,10 +7,6 @@ const options = {
     'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
   }
 };
-
-// const weight = (req.params.weight / 2.205)
-// const height = (req.params.height * 2.54)
-
 
 async function oneRepMax(e) {
   e.preventDefault();
@@ -32,71 +30,39 @@ function bmiInput(event) {
   fetch(url, options)
   .then(res => res.json())
   .then(json => {
-    
     console.log(json)
 
-    $("#bmiCont").empty()
-    $("#bmiCont").append(bmiDiv)
-    
-    
-    const bmiContent = 
-    `<div class="bmiCard">
-    <p class="ageValue">${json.data.bmi}</p>
-    <p class="heightValue">${json.data.health}</p>
-    <p class="weightValue">${json.data.healthy_bmi_range}</p>
-    </div>`
-
-    console.log(bmiContent)
-
-    $("#bmiWrap").append(bmiContent)
-})}
-
-// async function basalMetabolicRate(e) {
-//   e.preventDefault();
-
-//   const height = document.querySelector('#height2').value;
-//   const weight = document.querySelector('#weight3').value;
-//   const age = document.querySelector('#age2').value;
-
-//   const bmr = BMR_men(weight, height, age)
-//   return bmr
-// }
-
-// async function idealBodyWeight(e) {
-//   e.preventDefault();
-
-//   const height = document.querySelector('#height3').value;
-//   const age = document.querySelector('#age3').value;
-
-//   const ibw =  IBW_men(height, age);
-//   return ibw
-// }
+    const gender = document.querySelector('#gender').value;
+    const activitylevel = document.querySelector('#activitylevel').value;
+    const calorieUrl = `https://fitness-calculator.p.rapidapi.com/dailycalorie?age=${age}&gender=${gender}&height=${height}&weight=${weight}&activitylevel=${activitylevel}`;
 
 
 
+    fetch(calorieUrl, options)
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error('error:' + err));
 
-// const healthDiv = `<div id="bmiInput"></div>`
-
-// async function newFormHandler(event) {
-//   event.preventDefault();
-//   const age = parseInt(document.querySelector('#age').value);
-//   const height = parseInt(document.querySelector('#height').value);
-//   const weight = parseInt(document.querySelector('#weight').value);
-//   const gender = document.querySelector('#gender').value;
-//   const neck = parseInt(document.querySelector('#neck').value);
-//   const waist = parseInt(document.querySelector('#waist').value);
-//   const hips = parseInt(document.querySelector('#hips').value);
-//   const activitylevel = document.querySelector('#activitylevel').value;
-//   window.location = `/bmi/${age}/${weight}/${height}`;
-
-// }
-
-
+      $("#bmiCont").empty()
+      $("#bmiCont").append(bmiDiv)
+      
+      const bmiContent = 
+      `<div class="bmiCard">
+      <p class="ageValue">${json.data.bmi}</p>
+      <p class="heightValue">${json.data.health}</p>
+      <p class="weightValue">${json.data.healthy_bmi_range}</p>
+      <p class="bmrValue">${json.data.bmr}</p>
+      <p class="maintainValue">${json.data.goals}</p>
+      </div>`
+  
+      console.log(bmiContent)
+  
+      $("#bmiWrap").append(bmiContent)
 
 
+  })
+  .catch(err => console.error('error:' + err));
+}
 
 
 $(".bmiInputs").on("submit", bmiInput);
-// document.querySelector(".ormInput").addEventListener("submit", newFormHandler);
-// document.querySelector(".bmrInput").addEventListener("submit", newFormHandler);
-// document.querySelector(".ibwInput").addEventListener("submit", newFormHandler);
