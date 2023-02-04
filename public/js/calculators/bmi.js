d3cf5f698f9f996ace0236e6a75cb6adbbb4f9b6
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-02-04 12:14:23 -0600

    added macro API

--- public/js/calculators/bmi.js
+++ public/js/calculators/bmi.js
@@ -11,7 +11,7 @@ async function oneRepMax(e) {
 
   const weight = document.querySelector('#weight2').value;
   const reps = document.querySelector('#reps').value;
-  
+
   const result = oneRepMax(weight, reps);
   return result
 }
@@ -25,23 +25,30 @@ function bmiInput(event) {
   const weight = parseInt(document.querySelector('#weight').value) / 2.205;
   let url = `https://fitness-calculator.p.rapidapi.com/bmi?age=${age}&weight=${weight}&height=${height}`;
   fetch(url, options)
-  .then(res => res.json())
-  .then(json => {
-    console.log(json)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json)
 
-    const gender = document.querySelector('#gender').value;
-    const activitylevel = document.querySelector('#activitylevel').value;
-    const calorieUrl = `https://fitness-calculator.p.rapidapi.com/dailycalorie?age=${age}&gender=${gender}&height=${height}&weight=${weight}&activitylevel=${activitylevel}`;
+      const gender = document.querySelector('#gender').value;
+      const activitylevel = document.querySelector('#activitylevel').value;
+      const calorieUrl = `https://fitness-calculator.p.rapidapi.com/dailycalorie?age=${age}&gender=${gender}&height=${height}&weight=${weight}&activitylevel=${activitylevel}`;
 
-    fetch(calorieUrl, options)
-      .then(res => res.json())
-      .then(json2 => {
+      fetch(calorieUrl, options)
+        .then(res => res.json())
+        .then(json2 => {
 
-        $("#bmiCont").empty()
-        $("#bmiCont").append(bmiDiv)
-        
-        const bmiContent = 
-        `<div class="stat-details">
+          const goals = document.querySelector('#goals').value;
+          const activitylevelSlice = document.querySelector('#activitylevel').value.slice(-1);
+          const macroUrl = `https://fitness-calculator.p.rapidapi.com/macrocalculator?age=${age}&gender=${gender}&height=${height}&weight=${weight}&activitylevel=${activitylevelSlice}&goal=${goals}`;
+console.log(activitylevelSlice);
+          fetch(macroUrl, options)
+            .then(res => res.json())
+            .then(json3 => {
+              $("#bmiCont").empty()
+              $("#bmiCont").append(bmiDiv)
+
+              const bmiContent =
+                `<div class="stat-details">
         <div class="stat-box">
         <h3>Your Current BMI: <span class="stat-output">${json.data.bmi}</span></h3>
         <h3>BMI Status: <span class="stat-output">${json.data.health}</span></h3>
@@ -54,19 +61,37 @@ function bmiInput(event) {
         <h3>Daily calories for mild weight gain: <span class="stat-output">${Math.round(json2.data.goals["Mild weight gain"]["calory"])} (.5lbs)</span></h3>
         <h3>Daily calories for weight gain: <span class="stat-output">${Math.round(json2.data.goals["Weight gain"]["calory"])} (1.1lbs)</span></h3>
         <h3>Daily calories for extreme weight gain: <span class="stat-output">${Math.round(json2.data.goals["Extreme weight gain"]["calory"])} (2.2lbs)</span></h3>
+        <h2>Balanced</h2>
+        <h3>Calories: <span class="stat-output">${Math.round(json3.data.calorie)}</span></h3>
+        <h3>Protein: <span class="stat-output">${Math.round(json3.data.balanced.protein)}</span></h3>
+        <h3>Fat: <span class="stat-output">${Math.round(json3.data.balanced.fat)}</span></h3>
+        <h3>Carbs: <span class="stat-output">${Math.round(json3.data.balanced.carbs)}</span></h3>
+        <h2>Lowfat</h2>
+        <h3>Protein: <span class="stat-output">${Math.round(json3.data.lowfat.protein)}</span></h3>
+        <h3>Fat: <span class="stat-output">${Math.round(json3.data.lowfat.fat)}</span></h3>
+        <h3>Carbs: <span class="stat-output">${Math.round(json3.data.lowfat.carbs)}</span></h3>
+        <h2>Low Carbs</h2>
+        <h3>Protein: <span class="stat-output">${Math.round(json3.data.lowcarbs.protein)}</span></h3>
+        <h3>Fat: <span class="stat-output">${Math.round(json3.data.lowcarbs.fat)}</span></h3>
+        <h3>Carbs: <span class="stat-output">${Math.round(json3.data.lowcarbs.carbs)}</span></h3>
+        <h2>High Protein</h2>
+        <h3>Protein: <span class="stat-output">${Math.round(json3.data.highprotein.protein)}</span></h3>
+        <h3>Fat: <span class="stat-output">${Math.round(json3.data.highprotein.fat)}</span></h3>
+        <h3>Carbs: <span class="stat-output">${Math.round(json3.data.highprotein.carbs)}</span></h3>
         </div>
         </div>`
 
-        console.log(json2)
-    
-        console.log(bmiContent)
-    
-        $("#bmiWrap").append(bmiContent)
+              console.log(json2)
+              console.log(json3)
+              console.log(bmiContent)
 
+              $("#bmiWrap").append(bmiContent)
 
-  })
-  .catch(err => console.error('error:' + err));
-})}
+            })
+        })
+        .catch(err => console.error('error:' + err));
+    })
+}
 
 
 
